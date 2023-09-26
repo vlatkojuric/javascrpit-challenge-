beaa35ec62400a01937f8d7f2da8a11d9c5488e2
Vlatko Juric <juricv78gmail.com>
2023-09-27 00:50:06 +0200

    finally done

--- js/index.js
+++ js/index.js
@@ -1,3 +1,18 @@
 console.clear();
 
 const bodyElement = document.querySelector('[data-js="body"]');
+const darkMode = document.querySelector('[data-js="dark-mode-button"]');
+const lightMode = document.querySelector('[data-js="light-mode-button"]');
+const toggleMode = document.querySelector('[data-js="toggle-button"]');
+
+darkMode.addEventListener("click", () => {
+  bodyElement.classList.toggle("dark");
+});
+
+lightMode.addEventListener("click", () => {
+  bodyElement.classList.remove("dark");
+});
+
+toggleMode.addEventListener("click", () => {
+  bodyElement.classList.toggle("dark");
+});
